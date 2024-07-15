510837bfc219a7f065231067c37a554010ef0512
HYPERVEX0109 <61754232+HYPERVEX0109@users.noreply.github.com>
2024-07-15 14:41:17 +0100

    Make.com scenario command created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,5 @@
 <x-layout href='/'>
     <x-slot:heading>Home Page</x-slot:heading>
 
-    <form action="{{ route('trigger.make.scenario') }}" method="GET">
-        @csrf
-        <button type="submit">Trigger Make.com Scenario</button>
-    </form>
-
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -41,9 +41,3 @@ Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
 
-Route::get('/trigger-make-scenario', function(){
-    Artisan::call('app:trigger-make-scenario');
-
-    return redirect()->back()->with('success', 'Triggered Make.com scenario successfully!');
-
-})->name('trigger.make.scenario');
